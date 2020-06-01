7ca774b00b23d64a70d7f3df1c8f164f32fb7094
Danakt Saushkin <danakt@protonmail.com>
2020-06-02 02:49:09 +0300

    Update vulnerable dependencies

--- dist/comments.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Babel = require("@babel/types");
-exports.createComment = function (statement) {
-    var value = statement.value;
-    var emptyExpression = Babel.jsxEmptyExpression();
-    emptyExpression.innerComments = [{ type: 'CommentBlock', value: value }];
-    return Babel.jsxExpressionContainer(emptyExpression);
-};
