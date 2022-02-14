396ba2f488dd9982130b48ded5f648eb850fccd2
CAndRyan <CAndRyan@users.noreply.github.com>
2022-02-14 13:00:25 -0500

    Fix tests written for old method signature

--- tests/unit.ts
+++ tests/unit.ts
@@ -21,8 +21,9 @@ describe('preProcessUnsupportedParserFeatures', () => {
       '<div id="{{id}}" data="{{data}}"><div>'
     ].forEach((template) => {
       test('should return template unchanged', () => {
-        const result = preProcessUnsupportedParserFeatures(template);
-        expect(result).toEqual(template);
+        const { template: templateResult, helpers: helpersResult } = preProcessUnsupportedParserFeatures(template);
+        expect(templateResult).toEqual(template);
+        expect(helpersResult.length).toEqual(0);
       });
     });
   });
@@ -34,34 +35,34 @@ describe('preProcessUnsupportedParserFeatures', () => {
         expectedTemplate: '<div id="{{idIfHelper isTrue}}"><div>',
         expectedHelpers: [`const idIfHelper = (isTrue) => isTrue ? 'id' : '';`]
       },
-      // {
-      //   template: '<div id="{{#if isTrue}}id{{/if}}"><div>',
-      //   expectedTemplate: '<div id="{{idIfHelper isTrue}}"><div>',
-      //   expectedHelpers: [`const idIfHelper = (isTrue) => isTrue ? 'id' : '';`]
-      // },
-      // {
-      //   template: '<div id = "{{#if isTrue}}id{{/if}}"><div>',
-      //   expectedTemplate: '<div id="{{idIfHelper isTrue}}"><div>',
-      //   expectedHelpers: [`const idIfHelper = (isTrue) => isTrue ? 'id' : '';`]
-      // },
-      // {
-      //   template: '<div class="{{#if isTrue}}is-true{{/if}} other-class"><div>',
-      //   expectedTemplate: '<div class="{{classIfHelper isTrue}}"><div>',
-      //   expectedHelpers: ["const classIfHelper = (isTrue) => { const leading = ''; const trailing = ' other-class'; return isTrue ? `${leading}id${trailing}` : `${leading}${trailing};"]
-      // },
-      // {
-      //   template: '<div id="{{#if isTrue}}{{id}}{{/if}}"><div>',
-      //   expectedTemplate: '<div id="{{idIfHelper isTrue id}}"><div>',
-      //   expectedHelpers: ["const idIfHelper = (isTrue, id) => isTrue ? `${id}` : '';"]
-      // },
-      // {
-      //   template: '<div id="{{#if isTrue}}{{id}}{{/if}}" title={{#unless isTrue}}title{{/if}}><div>',
-      //   expectedTemplate: '<div id="{{idIfHelper isTrue id}}" title="{{titleUnlessHelper isTrue}}"><div>',
-      //   expectedHelpers: [
-      //     "const idIfHelper = (isTrue, id) => isTrue ? `${id}` : '';",
-      //     "const titleUnlessHelper = (isTrue) => isTrue ? 'title' : '';"
-      //   ]
-      // }
+      {
+        template: '<div id="{{#if isTrue}}id{{/if}}"><div>',
+        expectedTemplate: '<div id="{{idIfHelper isTrue}}"><div>',
+        expectedHelpers: [`const idIfHelper = (isTrue) => isTrue ? 'id' : '';`]
+      },
+      {
+        template: '<div id = "{{#if isTrue}}id{{/if}}"><div>',
+        expectedTemplate: '<div id="{{idIfHelper isTrue}}"><div>',
+        expectedHelpers: [`const idIfHelper = (isTrue) => isTrue ? 'id' : '';`]
+      },
+      {
+        template: '<div class="{{#if isTrue}}is-true{{/if}} other-class"><div>',
+        expectedTemplate: '<div class="{{classIfHelper isTrue}}"><div>',
+        expectedHelpers: ["const classIfHelper = (isTrue) => { const leading = ''; const trailing = ' other-class'; return isTrue ? `${leading}id${trailing}` : `${leading}${trailing};"]
+      },
+      {
+        template: '<div id="{{#if isTrue}}{{id}}{{/if}}"><div>',
+        expectedTemplate: '<div id="{{idIfHelper isTrue id}}"><div>',
+        expectedHelpers: ["const idIfHelper = (isTrue, id) => isTrue ? `${id}` : '';"]
+      },
+      {
+        template: '<div id="{{#if isTrue}}{{id}}{{/if}}" title={{#unless isTrue}}title{{/if}}><div>',
+        expectedTemplate: '<div id="{{idIfHelper isTrue id}}" title="{{titleUnlessHelper isTrue}}"><div>',
+        expectedHelpers: [
+          "const idIfHelper = (isTrue, id) => isTrue ? `${id}` : '';",
+          "const titleUnlessHelper = (isTrue) => isTrue ? 'title' : '';"
+        ]
+      }
     ].forEach(({ template, expectedTemplate, expectedHelpers }) => {
       test('should return template with helper functions', () => {
         const { template: templateResult, helpers: helpersResult } = preProcessUnsupportedParserFeatures(template);
