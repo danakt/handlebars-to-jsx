32a07c5553eefaf72a6de03ea3d4707ad77edfaf
CAndRyan <CAndRyan@users.noreply.github.com>
2022-02-18 13:14:02 -0500

    Add comment for generateAttributes helper to be included with templates using experimental features

--- src/helpers/generateAttributes.js
+++ src/helpers/generateAttributes.js
@@ -1,3 +1,8 @@
+/*
+* To be included alongside any JSX generated from a Handlebars template that conditionally renders attributes from it's own helper function
+* e.g. '<div {{getAttributesString innerContext}}></div>' becomes 'props => <div {...generateAttributes(getAttributesString(props.innerContext))}></div>'
+*/
+
 const getAllAttributesFromHtmlRegex = /([\w|'|"]*\s*=\s*[\w|'|"]*)/g;
 
 const generateAttributes = (dataString) => [...dataString.matchAll(getAllAttributesFromHtmlRegex)].reduce((aggregate, [_, match]) => {
